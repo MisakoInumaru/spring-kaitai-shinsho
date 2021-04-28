5a6cbc3b4e6dd3790e53f2bd194b506e15cffb31
MisakoInumaru <inumaru@ap-com.co.jp>
2021-04-28 07:50:14 +0000

    hello world

---
+++ kaitai-shinsho/src/test/java/com/example/kaitaishinsho/KaitaiShinshoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaitaishinsho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaitaiShinshoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
