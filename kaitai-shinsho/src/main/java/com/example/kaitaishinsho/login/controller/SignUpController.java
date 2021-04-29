b0b54b3adae9eea4818e9a7c8aa188d34d386eec
MisakoInumaru <inumaru@ap-com.co.jp>
2021-04-29 12:56:26 +0000

    バリデーションを実装

--- kaitai-shinsho/src/main/java/com/example/kaitaishinsho/login/controller/SignUpController.java
+++ kaitai-shinsho/src/main/java/com/example/kaitaishinsho/login/controller/SignUpController.java
@@ -4,6 +4,7 @@ import com.example.kaitaishinsho.login.controller.domain.model.SignUpForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +37,7 @@ public class SignUpController {
 	}
 
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute SignUpForm form,
+	public String postSignUp(@ModelAttribute @Validated SignUpForm form,
 	                         BindingResult bindingResult, Model model) {
 
 		if (bindingResult.hasErrors()) {
