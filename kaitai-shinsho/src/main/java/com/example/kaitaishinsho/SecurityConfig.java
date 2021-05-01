19a5ef8f14126c308391b05f0fe1172b0e74fb4c
MisakoInumaru <inumaru@ap-com.co.jp>
2021-05-01 12:36:06 +0000

    ログイン認証を追加

--- kaitai-shinsho/src/main/java/com/example/kaitaishinsho/SecurityConfig.java
+++ kaitai-shinsho/src/main/java/com/example/kaitaishinsho/SecurityConfig.java
@@ -1,17 +1,45 @@
 package com.example.kaitaishinsho;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	private DataSource dataSource;
+
+	private static final String USER_SQL = "SELECT"
+			+ " user_id,"
+			+ " password,"
+			+ " true"
+			+ " FROM m_user"
+			+ " WHERE user_id = ?";
+
+	private static final String ROLE_SQL = "SELECT"
+			+ " user_id,"
+			+ " role"
+			+ " FROM m_user"
+			+ " WHERE user_id = ?";
+
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/webjars/**", "/css/**");
+		web.ignoring().antMatchers("/webjars/**", "/css/**", "/h2-console/**");
 	}
 
 	@Override
@@ -19,10 +47,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 				.antMatchers("/webjars/**").permitAll()
 				.antMatchers("/css/**").permitAll()
+				.antMatchers("/h2-console/**").permitAll()
 				.antMatchers("/login").permitAll()
 				.antMatchers("/signup").permitAll()
 				.anyRequest().authenticated();
 
+		http.formLogin()
+				.loginProcessingUrl("/login")
+				.loginPage("/login")
+				.failureUrl("/login")
+				.usernameParameter("userId")
+				.passwordParameter("password")
+				.defaultSuccessUrl("/home", true);
+
 		http.csrf().disable();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery(USER_SQL)
+				.authoritiesByUsernameQuery(ROLE_SQL);
 	}
 }
--- kaitai-shinsho/src/main/resources/data.sql
+++ kaitai-shinsho/src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO employee (employee_id, employee_name, age)
 VALUES(1, 'yamada', 30);
 
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
-VALUES('yamada@xxx.co.jp','password', 'yamada', '1990-01-01', 30, false, 'ROLE_ADMIN');
+VALUES('yamada@xxx.co.jp','$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', 'yamada', '1990-01-01', 30, false, 'ROLE_ADMIN');
 
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
-VALUES('tamura@xxx.co.jp','password', 'tamura', '1990-01-01', 30, false, 'ROLE_GENERAL');
+VALUES('tamura@xxx.co.jp','$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', 'tamura', '1990-01-01', 30, false, 'ROLE_GENERAL');
