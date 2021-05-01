731fb24043838e8d49d0b5ecca55dbb6e3f120fb
MisakoInumaru <inumaru@ap-com.co.jp>
2021-05-01 13:25:09 +0000

    GETメソッドを実装

--- kaitai-shinsho/src/main/java/com/example/kaitaishinsho/login/controller/UserRestController.java
+++ kaitai-shinsho/src/main/java/com/example/kaitaishinsho/login/controller/UserRestController.java
@@ -1,14 +1,27 @@
 package com.example.kaitaishinsho.login.controller;
 
+import com.example.kaitaishinsho.login.domain.model.User;
 import com.example.kaitaishinsho.login.domain.service.RestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserRestController {
 
 	@Autowired
-	RestService restService;
+	RestService service;
 
+	@GetMapping("/rest/get")
+	public List<User> getUserMany() {
+		return service.selectMany();
+	}
 
+	@GetMapping("/rest/get/{id:.+}")
+	public User getUserOne(@PathVariable("id") String userId) {
+		return service.selectOne(userId);
+	}
 }
