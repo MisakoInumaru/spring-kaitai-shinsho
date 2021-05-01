7e861955befd082e608ecf1b7156f1f39a028def
MisakoInumaru <inumaru@ap-com.co.jp>
2021-05-01 08:42:53 +0000

    エラーハンドラーを追加

--- kaitai-shinsho/src/main/java/com/example/kaitaishinsho/login/controller/SignUpController.java
+++ kaitai-shinsho/src/main/java/com/example/kaitaishinsho/login/controller/SignUpController.java
@@ -3,11 +3,15 @@ package com.example.kaitaishinsho.login.controller;
 import com.example.kaitaishinsho.login.domain.model.SignUpForm;
 import com.example.kaitaishinsho.login.domain.model.User;
 import com.example.kaitaishinsho.login.domain.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
