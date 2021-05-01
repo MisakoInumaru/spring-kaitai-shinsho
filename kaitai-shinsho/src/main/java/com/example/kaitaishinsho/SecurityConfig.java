5b4210eb044a5929cbd862a79b391f294cb50be3
MisakoInumaru <inumaru@ap-com.co.jp>
2021-05-01 10:49:32 +0000

    直リンクを禁止

---
+++ kaitai-shinsho/src/main/java/com/example/kaitaishinsho/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.kaitaishinsho;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/webjars/**", "/css/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/webjars/**").permitAll()
+				.antMatchers("/css/**").permitAll()
+				.antMatchers("/login").permitAll()
+				.antMatchers("/signup").permitAll()
+				.anyRequest().authenticated();
+
+		http.csrf().disable();
+	}
+}
